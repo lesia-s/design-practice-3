250292928efcd2ceea71ed0c9945d0cbe8dbcdbb
lesia-s <lesiiasergienko@gmail.com>
2020-05-09 19:30:16 +0300

    Unit tests

--- cmd/lb/balancer_test.go
+++ cmd/lb/balancer_test.go
@@ -1,7 +1,37 @@
 package main
 
-import "testing"
+import (
+  "testing"
+  . "gopkg.in/check.v1"
+)
 
-func TestBalancer(t *testing.T) {
-	// TODO: Реалізуйте юніт-тест для балансувальникка.
+func Test(t *testing.T) { TestingT(t) }
+
+type MySuite struct{}
+
+var _ = Suite(&MySuite{})
+
+var tests = [][3]struct {
+  health bool
+  connections int
+}{
+  {{true, 1}, {true, 2}, {true, 3}},
+  {{true, 2}, {true, 3}, {true, 1}},
+  {{true, 3}, {true, 1}, {true, 2}},
+  {{false, 1}, {true, 2}, {true, 3}},
+  {{false, 1}, {false, 2}, {true, 3}},
+  {{false, 1}, {false, 1}, {false, 1}},
+}
+
+var expIndex = []int{0, 2, 1, 1, 2, -1}
+
+func (s *MySuite) TestBalancer(c *C) {
+  for j, test := range tests {
+    for i, inServer := range test {
+      server := serversPool[i]
+      (*server).isHealthy = inServer.health
+      (*server).connCnt = inServer.connections
+    }
+    c.Assert(expIndex[j], Equals, findMin(serversPool))
+	}
 }
